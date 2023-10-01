ecd918e6e469d0a7acd2e27374e46dee300a26fa
iZnoG0uD <zelesnac@gmail.com>
2023-10-01 18:53:45 +0400

    Добавьте файлы проекта.

---
+++ Module5SF/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Module5SF
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            (string Name, string[] Dishes) User;
+            User.Dishes = new string[5];
+
+            Console.Write("\tЗдравствуйте! Введите Ваше имя: ");
+            User.Name = Console.ReadLine();
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write("\tВведите Ваше любимое блюдо: ");
+                User.Dishes[i] = Console.ReadLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
